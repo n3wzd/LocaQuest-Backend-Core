3fe329ae183f90e17bf2372a8c286c0abe985765
n3wzd <87504447+n3wzd@users.noreply.github.com>
2025-01-12 19:29:34 +0900

    create project files

---
+++ src/main/java/com/example/reviewsplash/ReviewsplashApplication.java
@@ -0,0 +1,13 @@
+package com.example.reviewsplash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewsplashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewsplashApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/reviewsplash/ReviewsplashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reviewsplash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewsplashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
